70518a2f601979b6536a6b0c674f3d31c5485710
rams211 <66926492+rams211@users.noreply.github.com>
2023-02-20 20:37:02 -0800

    pushing files from API

---
+++ javacode/src/main/java/com/ezapi/api/service/impl/ProductServiceImpl.java
@@ -0,0 +1,78 @@
+package com.ezapi.api.service.impl;
+
+import com.ezapi.api.domain.Product;
+import com.ezapi.api.repository.ProductRepository;
+import com.ezapi.api.service.ProductService;
+import com.ezapi.api.service.dto.ProductDTO;
+import com.ezapi.api.service.mapper.ProductMapper;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Service Implementation for managing {@link Product}.
+ */
+@Service
+@Transactional
+public class ProductServiceImpl implements ProductService {
+
+    private final Logger log = LoggerFactory.getLogger(ProductServiceImpl.class);
+
+    private final ProductRepository productRepository;
+
+    private final ProductMapper productMapper;
+
+    public ProductServiceImpl(ProductRepository productRepository, ProductMapper productMapper) {
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    @Override
+    public ProductDTO save(ProductDTO productDTO) {
+        log.debug("Request to save Product : {}", productDTO);
+        Product product = productMapper.toEntity(productDTO);
+        product = productRepository.save(product);
+        return productMapper.toDto(product);
+    }
+
+    @Override
+    public Optional<ProductDTO> partialUpdate(ProductDTO productDTO) {
+        log.debug("Request to partially update Product : {}", productDTO);
+
+        return productRepository
+            .findById(productDTO.getId())
+            .map(
+                existingProduct -> {
+                    productMapper.partialUpdate(existingProduct, productDTO);
+                    return existingProduct;
+                }
+            )
+            .map(productRepository::save)
+            .map(productMapper::toDto);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public List<ProductDTO> findAll() {
+        log.debug("Request to get all Products");
+        return productRepository.findAll().stream().map(productMapper::toDto).collect(Collectors.toCollection(LinkedList::new));
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Optional<ProductDTO> findOne(Long id) {
+        log.debug("Request to get Product : {}", id);
+        return productRepository.findById(id).map(productMapper::toDto);
+    }
+
+    @Override
+    public void delete(Long id) {
+        log.debug("Request to delete Product : {}", id);
+        productRepository.deleteById(id);
+    }
+}
