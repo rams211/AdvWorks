63e5f649ef399f65eef378f5a44e94eaf1252853
rams211 <66926492+rams211@users.noreply.github.com>
2023-02-20 20:36:32 -0800

    pushing files from API

---
+++ javacode/src/main/java/com/ezapi/api/domain/Product.java
@@ -0,0 +1,80 @@
+package com.ezapi.api.domain;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+/**
+ * A Product.
+ */
+@Entity
+@Table(name = "product")
+@Cache(usage = CacheConcurrencyStrategy.READ_WRITE)
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @Column(name = "productid")
+    private String productid;
+
+    // jhipster-needle-entity-add-field - JHipster will add fields here
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Product id(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getProductid() {
+        return this.productid;
+    }
+
+    public Product productid(String productid) {
+        this.productid = productid;
+        return this;
+    }
+
+    public void setProductid(String productid) {
+        this.productid = productid;
+    }
+
+    // jhipster-needle-entity-add-getters-setters - JHipster will add getters and setters here
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        return id != null && id.equals(((Product) o).id);
+    }
+
+    @Override
+    public int hashCode() {
+        // see https://vladmihalcea.com/how-to-implement-equals-and-hashcode-using-the-jpa-entity-identifier/
+        return getClass().hashCode();
+    }
+
+    // prettier-ignore
+    @Override
+    public String toString() {
+        return "Product{" +
+            "id=" + getId() +
+            ", productid='" + getProductid() + "'" +
+            "}";
+    }
+}
